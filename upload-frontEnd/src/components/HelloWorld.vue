c1231dc3e2732aa69d3708d05e7d4aff6a32afec
gaojiaxing <gaojiaxing@xiaohongshu.com>
2023-07-24 20:57:54 +0800

    feat: 上传文件上传前端

---
+++ upload-frontEnd/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import axios from "axios";
+
+const fileChange = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  console.log(target.files);
+}
+
+const submit = () => {
+  console.log('submit');
+
+}
+
+</script> 
+
+<template>
+  <input type="file" name="file" @change="fileChange">
+  <button @click="submit">提交</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
